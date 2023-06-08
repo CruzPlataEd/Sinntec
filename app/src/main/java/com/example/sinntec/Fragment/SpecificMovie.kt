576c47e7810e6c4a374b328b6141486d8c20df03
Cruz Plata Eduardo <cruzplata.eduardo@gmail.com>
2023-06-08 13:15:49 -0600

    Implementación de nav_graph y segundoView

--- app/src/main/java/com/example/sinntec/Fragment/SpecificMovie.kt
+++ app/src/main/java/com/example/sinntec/Fragment/SpecificMovie.kt
@@ -5,12 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.sinntec.R
+import com.example.sinntec.databinding.FragmentSpecificMovieBinding
+import com.squareup.picasso.Picasso
 
 class SpecificMovie : Fragment() {
 
+    private var _binding: FragmentSpecificMovieBinding?= null
+    private val binding get() = checkNotNull(_binding){ "Cannot access binding because it is null. Is the view visible?" }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_specific_movie, container, false)
+        _binding = FragmentSpecificMovieBinding.inflate(inflater,container,false)
+
+        val safeArgs: SpecificMovieArgs by navArgs()
+        val overview = safeArgs.overview
+        val title = safeArgs.title
+        val backdrop = safeArgs.backdrop
+        val calificacion = safeArgs.calificacion
+
+        Picasso.get().load("https://image.tmdb.org/t/p/w500/${backdrop}").into(binding.imagenBack)
+        binding.titulo.text = title
+        binding.overview.text = overview
+        binding.ratingBar.rating = (calificacion/2)
+        binding.calificacion.text = "Rating: ${calificacion}"
+        return binding.root
     }
 }
